d5a3d2a32e58100decfbdc4029dbb17db37aaa14
codewithrohith <rohithsaicharaneanugula@gmail.com>
2025-02-17 13:47:26 -0600

    minor fix

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY", "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")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
 UPLOAD_FOLDER = "backend/uploads"
